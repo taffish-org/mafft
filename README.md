e308092481e991fddf0b60f8974d3d60ac14881d
Kaiyuan Han <hermit_han@163.com>
2025-03-13 18:09:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-mafft
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://mafft.cbrc.jp/alignment/software/source.html
+
+Multiple alignment program for amino acid or nucleotide sequences.
---
+++ mafft.info.lisp
@@ -0,0 +1,27 @@
+(:name           "mafft")
+(:repo           "taffish-org/mafft")
+(:author         "KaiyuanHan")
+(:version        "7.525")
+(:intro          "Multiple alignment program for amino acid or nucleotide sequences")
+(:dependencies)
+(:functions      "mafft"
+                 "mafft-distance"
+                 "mafft-einsi"
+                 "mafft-fftns"
+                 "mafft-fftnsi"
+                 "mafft-ginsi"
+                 "mafft-homologs.rb"
+                 "mafft-linsi"
+                 "mafft-nwns"
+                 "mafft-nwnsi"
+                 "mafft-profile"
+                 "mafft-qinsi"
+                 "mafft-sparsecore.rb"
+                 "mafft-xinsi"
+                 "einsi"
+                 "fftns"
+                 "fftnsi"
+                 "ginsi"
+                 "linsi"
+                 "nwns"
+                 "nwnsi")
